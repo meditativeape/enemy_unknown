415c786beec5f33d8e5184f22ddf4e852e5d458f
Frank Zhang <immefrank@163.com>
2013-01-20 17:11:08 -0600

    Unit attack

--- unit.js
+++ unit.js
@@ -7,7 +7,7 @@
 function Unit(/*int*/ team,/*int*/hp,/*int*/ type, /*Coordinate*/cord, /*int*/stat, /*int*/ cooldown, /*image*/pic){
 	this.team = team;
 	this.hp = hp;
-	this.type = type;
+	this.type = type; //0. Wood 1. Water. 2.Earth. 3.Fire 4.Air undefined.Unknown
 	this.x = cord.X;
 	this.y = cord.Y;
 	this.status = stat;
@@ -23,4 +23,26 @@ Unit.prototype.draw = function(/*Point*/p, /*int*/height) {
 	var ctx = document.getElementById('gameCanvas').getContext('2d');
 	ctx.drawImage(this.image, p.X - this.image.width/2, p.Y + height/4 - this.image.height, 
 		this.image.width, this.image.height);
+};
+
+Unit.prototype.gotHit = function(/*Unit*/enemy){
+	//Wood beats water and earth
+	//Water beats earth and fire
+	//Earth beats fire and air
+	//Fire beats air and wood
+	//Air beats wood and water
+	var damgage = 10;
+	//Calculate type advantage
+    var flag = (this.type-enemy.type)%5
+	//Enemy advantage
+    if (flag>2):
+    	this.hp = this.hp  
+		enemy.hp = enemy.hp
+	//My advantage
+    elif (flag!=0):
+        this.hp = this.hp
+		enemy.hp = enemy.hp
+    else:
+        this.hp = this.hp
+		enemy.hp = enemy.hp
 };
