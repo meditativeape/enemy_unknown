ab7accf4618e252f7d9373a4acac6e7beaf5774b
Beck Chen <csybeck@gmail.com>
2013-02-17 15:19:14 -0600

    Server will send endGame message when a team wins.

--- game.core.server.js
+++ game.core.server.js
@@ -32,6 +32,8 @@ String.prototype.format.regex = new RegExp("{-?[0-9]+}", "g");
 
         // Store the players
         this.players = playerList;
+		// Store the number of units for each team;
+		this.units = [];
 		// Store game's uuid
 		this.id = gameid;
 		// Store the type of the game. One of 0, 1, 2, 3.
@@ -79,10 +81,14 @@ String.prototype.format.regex = new RegExp("{-?[0-9]+}", "g");
 		var unit2 = this.hexgrid.getUnit(coord2);
 		this.hexgrid.attack(coord1, coord2);
 		responses.push(["1", "attack", coord1.X, coord1.Y, unit1.hp, coord2.X, coord2.Y, unit2.hp].join(" "));
-		if (unit1.hp <= 0)  // unit 1 dies
+		if (unit1.hp <= 0) {  // unit 1 dies
 			responses.push(["1", "die", coord1.X, coord1.Y, unit1.type].join(" "));
-		if (unit2.hp <= 0)  // unit 2 dies
+			this.units[unit1.player]--;
+		}
+		if (unit2.hp <= 0) {  // unit 2 dies
 			responses.push(["1", "die", coord2.X, coord2.Y, unit2.type].join(" "));
+			this.units[unit2.player]--;
+		}
 		return responses;
 		
 	};
@@ -147,6 +153,7 @@ String.prototype.format.regex = new RegExp("{-?[0-9]+}", "g");
 						for (var j in responses)
 							this.sendMsg(this.players[i], responses[j]);
 					}
+					this.checkGameStatus();
 				}
 				break;
 			default:
@@ -180,6 +187,18 @@ String.prototype.format.regex = new RegExp("{-?[0-9]+}", "g");
 		default:
 			// TODO: send response about invalid message
 		}
+	};
+	
+	game_core_server.prototype.checkGameStatus = function(){
+		var winningTeam = -1;
+		for (var i in this.units) {
+			if (this.units[i] > 0)
+				if (winningTeam == -1)
+					return;
+				else
+					winningTeam = i;
+		}
+		this.endGame(winningTeam);  // some team wins
 	};
 	
 	game_core_server.prototype.sendMsg = function(recipient, message){
@@ -219,22 +238,24 @@ String.prototype.format.regex = new RegExp("{-?[0-9]+}", "g");
 		pieces.push(this.hexgrid.matrix[2][2].piece = new Unit(0, 0, 100, types[2], new helper.Coordinate(2, 2), 0, null));
 		pieces.push(this.hexgrid.matrix[3][1].piece = new Unit(0, 0, 100, types[3], new helper.Coordinate(3, 1), 0, null));
 		pieces.push(this.hexgrid.matrix[4][0].piece = new Unit(0, 0, 100, types[4], new helper.Coordinate(4, 0), 0, null));
+		this.units.push(5);
 		shuffle(types);
 		pieces.push(this.hexgrid.matrix[6][10].piece = new Unit(1, 1, 100, types[0], new helper.Coordinate(6, 10), 0, null));
 		pieces.push(this.hexgrid.matrix[7][9].piece = new Unit(1, 1, 100, types[1], new helper.Coordinate(7, 9), 0, null));
 		pieces.push(this.hexgrid.matrix[8][8].piece = new Unit(1, 1, 100, types[2], new helper.Coordinate(8, 8), 0, null));
 		pieces.push(this.hexgrid.matrix[9][7].piece = new Unit(1, 1, 100, types[3], new helper.Coordinate(9, 7), 0, null));
 		pieces.push(this.hexgrid.matrix[10][6].piece = new Unit(1, 1, 100, types[4], new helper.Coordinate(10, 6), 0, null));
+		this.units.push(5);
 		// 3 players
-		if (this.type == 1) {
-			pieces.push(this.hexgrid.matrix[0][5].piece = new Unit(2, 2, 100, randomType(), new helper.Coordinate(0, 5), 0, null));
-			pieces.push(this.hexgrid.matrix[1][4].piece = new Unit(2, 2, 100, randomType(), new helper.Coordinate(1, 4), 0, null));
-		}
+		// if (this.type == 1) {
+			// pieces.push(this.hexgrid.matrix[0][5].piece = new Unit(2, 2, 100, randomType(), new helper.Coordinate(0, 5), 0, null));
+			// pieces.push(this.hexgrid.matrix[1][4].piece = new Unit(2, 2, 100, randomType(), new helper.Coordinate(1, 4), 0, null));
+		// }
 		// 4 players
-		if (this.type == 2) {
-			pieces.push(this.hexgrid.matrix[5][1].piece = new Unit(3, 3, 100, randomType(), new helper.Coordinate(5, 1), 0, null));
-			pieces.push(this.hexgrid.matrix[6][0].piece = new Unit(3, 3, 100, randomType(), new helper.Coordinate(6, 0), 0, null));
-		}
+		// if (this.type == 2) {
+			// pieces.push(this.hexgrid.matrix[5][1].piece = new Unit(3, 3, 100, randomType(), new helper.Coordinate(5, 1), 0, null));
+			// pieces.push(this.hexgrid.matrix[6][0].piece = new Unit(3, 3, 100, randomType(), new helper.Coordinate(6, 0), 0, null));
+		// }
 		
 		// do not work for 2v2 yet
 		var k = 0;
@@ -250,7 +271,7 @@ String.prototype.format.regex = new RegExp("{-?[0-9]+}", "g");
 		
 		console.log(":: " + this.id.substring(0,8) + " :: Game started!");
 		for (var i in this.players) {
-			this.sendMsg(this.players[i], "0 start {0} {1}".format([pieces[5*i].x, pieces[5*i].y]));
+			this.sendMsg(this.players[i], "0 start {0} {1} {2}".format([pieces[5*i].x, pieces[5*i].y]), this.type);
 		}
 	};
 	
@@ -267,9 +288,14 @@ String.prototype.format.regex = new RegExp("{-?[0-9]+}", "g");
 		console.log(":: " + this.id.substring(0,8) + " :: player " + client.userid.substring(0,8) + " has left the game.");
 		// if this game has no more than one player, end it
 		if (this.players.length == 1) {
-			this.sendMsg(this.players[0], "0 end " + this.players[0].userid);
-			this.players[0].game = null;
+			this.endGame(this.players[0].team);
 		}
-		if (this.players.length <= 1)
-			this.server.endGame(this.id);
+	};
+	
+	game_core_server.prototype.endGame = function(winningTeam){
+		for (var i in this.players) {
+			this.sendMsg(this.players[i], "0 end " + winningTeam);
+			this.players[i].game = null;
+		}
+		this.server.endGame(this.id)
 	};
