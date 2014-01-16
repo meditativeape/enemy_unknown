91a9cb335ce1200c494a12d5f6b1846925eec1e6
Beck Chen <csybeck@gmail.com>
2014-01-16 14:18:46 -0600

    Refactor hexgrid code: add many helper functions; cleanup; fix wrong function calls.

--- shared/game.shared.hexgrid.js
+++ shared/game.shared.hexgrid.js
@@ -1,5 +1,5 @@
 /**
- * Hex grid map objects and helper functions.
+ * Hexgrid and hexagon objects shared by both client and server.
  */
  
 /**
@@ -15,211 +15,123 @@ if( 'undefined' !== typeof global ) {
 }
 
 /**
- * Constructor for a shared hexgird. Automatically builds matrix of hexagons.
- * @constructor
+ * Creates a shared hexgird.
+ * Automatically builds a matrix of hexagons based on the given scenario.
  */
-var Hexgrid = function(/*string*/ mapName, /*boolean*/ fogOn){
+var Hexgrid = function(/*string*/ scenarioName, /*boolean*/ fogOn){
 
-	this.scenario = Scenarios[mapName];
+	this.scenario = Scenarios[scenarioName];
     this.fogOn = fogOn;
+    this.numRows = this.scenario.size.numRows;
+	this.numCols = this.scenario.size.numCols;
+    
+    // build the matrix of hexagons
 	this.matrix = [];
-    var numRows = this.scenario.size.numRows;
-	var numCols = this.scenario.size.numCols;
-	for (var i = 0; i < numRows; i++) {
+	for (var i = 0; i < this.numRows; i++) {
 		this.matrix[i] = [];
-		for (var j = 0; j < numCols; j++) {
+		for (var j = 0; j < this.numCols; j++) {
 			this.matrix[i][j] = new Hexagon(new Coordinate(i, j));
 		}
-	}		
-
-// Moved to view
-//	while(xpos < x/2 && (ypos - spec.height/2>0)){
-//		while(matrixx <= rows && (ypos + spec.height/2<y)){
-//			var id = matrixx.toString(10) + matrixy.toString(10);
-//			if(matrixy == 0){
-//				this.matrix[matrixx] = [];
-//				if(xpos + spec.width/2 + spec.side/2 < x/2){
-//					rows =  rows + 1;
-//				}			
-//			}
-//			var hexagon = new Hexagon(id, matrixx, matrixy, xpos, ypos, spec, camera, this, callback, fogOn, fogImg);
-//			this.matrix[matrixx][matrixy] = hexagon;
-//			ypos = ypos + spec.height/2;
-//			xpos = xpos + spec.width/2 + spec.side/2;
-//			matrixx = matrixx + 1;
-//		}
-//		matrixx = 0;
-//		matrixy = matrixy + 1;
-//		ypos = y/2 - spec.height/2*(matrixy+1);
-//		xpos = offset+ (spec.width/2 + spec.side/2)*matrixy;
-//	}
-	
-	//Moved to View
-	// draw hexagons, terrains, units, and hp bars, then update them
-//	if (camera && layer) {
-//		layer.add(this.terrainGroup);
-//		layer.add(this.hexGroup);
-//		layer.add(this.unitGroup);
-//        layer.add(this.fogGroup);
-//		var me = this;
-//		this.anim = new Kinetic.Animation(function(frame) {
-//			for(var x in me.matrix){
-//				for(var y in me.matrix[x]){
-//					me.matrix[x][y].update();
-//				}
-//			}
-//		}, layer);
-//		this.anim.start();
-//	}
-	
-	//View
-//	this.stop = function stop(){
-//		if (this.anim)
-//			this.anim.stop();
-//	}
-	
-	//View
-//	this.toHex = function toHex(/*Point*/ p){
-//		p.X += this.camera.x;
-//		p.Y += this.camera.y;
-//		for(var x in this.matrix){
-//			for(var y in this.matrix[x]){
-//				if (this.matrix[x][y].contains(p))
-//					return new Coordinate(x,y);
-//			}
-//		}	
-//		return null;
-//	};
-	
-//	this.toMap = function toMap(/*Coordinate*/ coord){
-//		return this.matrix[coord.X][coord.Y].MidPoint;
-//	};
-	
-	this.hexDist = function hexDist(/*Hexagon*/ h1, /*Hexagon*/ h2) {
-		var deltaX = h1.matrixx - h2.matrixx;
-		var deltaY = h1.matrixy - h2.matrixy;
-		return ((Math.abs(deltaX) + Math.abs(deltaY) + Math.abs(deltaX + deltaY)) / 2);		
-	};
-	
-	this.move = function(/*Coordinate*/ origin, /*Coordinate*/dest) {
-		var toMove = this.matrix[origin.X][origin.Y].piece;
-		this.matrix[origin.X][origin.Y].piece = null;
-		this.matrix[dest.X][dest.Y].piece = toMove;
-        toMove.x = dest.X;
-        toMove.y = dest.Y;
-	};
-	
-	this.attack = function(/*Coordinate*/ attacker, /*Coordinate*/gothit){
-		this.matrix[gothit.X][gothit.Y].piece.gotHit(this.matrix[attacker.X][attacker.Y].piece);
-		if(this.matrix[gothit.X][gothit.Y].piece.hp<=0){
-			this.matrix[gothit.X][gothit.Y].piece = null;
-		}
-		if(this.matrix[attacker.X][attacker.Y].piece.hp<=0){
-			this.matrix[attacker.X][attacker.Y].piece = null;
-		}
-	}
-	
-	this.getUnit = function(/*Coordinate*/toCheck){
-        if (this.matrix[toCheck.X] && this.matrix[toCheck.X][toCheck.Y])
-            return this.matrix[toCheck.X][toCheck.Y].piece;
-        else
-            return null;
-	};
-	
-	this.addUnit = function(/*Unit */ toAdd, /*Coordinate*/dest){
-		this.matrix[dest.X][dest.Y].piece = toAdd;
-	};
-	
-	this.getTerrain = function(/*Coordinate*/toCheck){
-		return this.matrix[toCheck.X][toCheck.Y].terrain;
-	};
-	
-	this.addTerrain = function(toAdd, /*Coordinate*/dest){
-		this.matrix[dest.X][dest.Y].terrain = toAdd;
 	}
 };
 
 /**
- * Constructs a hexagon.
- * camera and fogImg could be null on server side, since no visualization is needed.
- * @constructor
+ * Checks if the hexgrid contains the given coordination.
  */
-function Hexagon(/*Coordinate*/coord) {
-	// original argument list: id, mx, my, x, y, spec, camera, map, callback, fogOn, fogImg
-	this.piece = null;
-    this.coord = coord;
-    
-    // Moved to view
-	//this.matrixx = mx;
-	//this.matrixy = my;
-    //if (fogOn != true)
-    //    this.clientViewable = true;
-    //this.opacity = 1;
-	//this.clientPastViewable = false;
-	//this.guessing = false;
-	//this.Points = [];//Polygon Base
-//	this.spec = spec;
-//	var x1 = (spec.width - spec.side)/2;
-//	var y1 = (spec.height / 2);
-//	this.Points.push(new Point(x1 + x, y));
-//	this.Points.push(new Point(x1 + spec.side + x, y));
-//	this.Points.push(new Point(spec.width + x, y1 + y));
-//	this.Points.push(new Point(x1 + spec.side + x, spec.height + y));
-//	this.Points.push(new Point(x1 + x, spec.height + y));
-//	this.Points.push(new Point(x, y1 + y));
+Hexgrid.prototype.containsCoord = function(/*Coordinate*/ coord) {
+    if (coord.X >= 0 && coord.X < numRows && coord.Y >= 0 && coord.Y < numCols)
+        return true;
+    else
+        return false;
+}
 
-//	this.Id = id;
-//	
-//	this.x = x;
-//	this.y = y;
-//	this.x1 = x1;
-//	this.y1 = y1;
-//	
-//	this.TopLeftPoint = new Point(this.x, this.y);
-//	this.BottomRightPoint = new Point(this.x + spec.width, this.y + spec.height);
-//	this.MidPoint = new Point(this.x + (spec.width / 2), this.y + y1);
-//	
-//	this.P1 = new Point(x + x1, y + y1);
-//	
-//	this.selected = false;
-	
-	//moved to View
-//	if (callback) {
-//		this.callback = callback;
-//	}
-//	
-//	if (camera) {
-//		this.camera = camera;
-//        this.fogImg = fogImg;
-//		
-//		// add hexagon
-//		var hexagonConfig = {
-//			points:[],
-//			stroke:'rgb(255,255,255)',
-//		};
-//		for (var i = 0; i < this.Points.length; i++) {
-//			hexagonConfig.points.push([this.Points[i].X-this.camera.x, this.Points[i].Y-this.camera.y]);
-//		}
-//		this.hexagonToDraw = new Kinetic.Polygon(hexagonConfig);
-//        
-//        // register event listener
-//        var me = this;
-//		if (this.callback) {
-//			this.hexagonToDraw.on('click tap', function(event){
-//               	me.callback(new Coordinate(me.matrixx, me.matrixy), event);
-//			});
-//		}
-//        this.hexagonToDraw.on('mouseover', function(){
-//            if (me.piece || me.reachable || me.clientAttackable || me.clientBuildable) {
-//                me.hexagonToDraw.setStroke("orange");
-//                me.hexagonToDraw.moveToTop();
-//                document.body.style.cursor = "pointer";
-//            }
-//        });
-//        this.hexagonToDraw.on('mouseout', function(){
-//            me.hexagonToDraw.setStroke("white");
-//            document.body.style.cursor = "auto";
-//        });
-//		this.map.hexGroup.add(this.hexagonToDraw);
-//	}
+/**
+ * Returns the hexagon at a given coordinate.
+ */
+Hexgrid.prototype.getHexagon = function(/*Coordinate*/ coord) {
+    if (this.containsCoord(coord))
+        return this.matrix[coord.X][coord.Y];
+    else
+        return null;
+};
+
+/**
+ * Returns the unit at a given coordinate.
+ */
+Hexgrid.prototype.getUnit = function(/*Coordinate*/ coord){
+    return this.getHexagon(coord).piece;
+};
+
+/**
+ * Sets the unit at a given coordinate.
+ */
+Hexgrid.prototype.setUnit = function(/*Unit*/ toSet, /*Coordinate*/ coord){
+    this.getHexagon(coord).piece = toSet;
+};
+
+/**
+ * Returns the terrain at a given coordinate.
+ */
+Hexgrid.prototype.getTerrain = function(/*Coordinate*/ toCheck){
+    return this.getHexagon(coord).terrain;
+};
+
+/**
+ * Sets the terrain at a given coordinate.
+ */
+Hexgrid.prototype.setTerrain = function(/*Terrain*/ toSet, /*Coordinate*/ coord){
+    this.getHexagon(coord).terrain = toSet;
+};
+
+/**
+ * Calculates the distance between two hexagons.
+ */
+Hexgrid.prototype.hexDist = function(/*Hexagon*/ h1, /*Hexagon*/ h2) {
+    return this.hexDistByCoord(h1.coord, h2.coord);		
+};
+
+/**
+ * Calculates the distance between two hexagons by coordinations.
+ */
+Hexgrid.prototype.hexDistByCoord = function(/*Coordination*/ coord1, /*Coordination*/ coord2) {
+    var deltaX = coord1.X - coord2.X;
+    var deltaY = coord1.Y - coord2.Y;
+    return ((Math.abs(deltaX) + Math.abs(deltaY) + Math.abs(deltaX + deltaY)) / 2);		
+};
+
+/**
+ * Move a unit from one hexagon to another hexagon.
+ * This method will not validate the move.
+ */
+Hexgrid.prototype.move = function(/*Coordinate*/ origin, /*Coordinate*/ dest) {
+    var toMove = this.getUnit(origin);
+    this.setUnit(null, origin);
+    this.setUnit(toMove, dest);
+    toMove.coord = dest;
+};
+
+/**
+ * Use a unit to attack another enemy's unit.
+ * This method will not validate the attack.
+ */
+Hexgrid.prototype.attack = function(/*Coordinate*/ attacker, /*Coordinate*/ enemy){
+    var attUnit = this.getUnit(attacker);
+    var enemyUnit = this.getUnit(enemy);
+    enemyUnit.gotHit(attUnit);
+    if (enemyUnit.hp <= 0){
+        this.setUnit(null, enemy);
+    }
+    if (attUnit.hp <= 0){
+        this.setUnit(null, attacker);
+    }
+};
+
+/**
+ * Constructs a hexagon.
+ * Contains the coordination of the hexagon, and the unit and terrain in the hexagon.
+ */
+function Hexagon(/*Coordinate*/ coord) {
+	this.piece = null;
+    this.terrain.null;
+    this.coord = coord;
 };
